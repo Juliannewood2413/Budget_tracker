4c9428b1db9f4f1d6c07c9b10fd7428ebdded017
Jules <juliannewood2413@gmail.com>
2021-02-08 13:58:47 -0700

    updated db.js

---
+++ public/db.js
@@ -0,0 +1,64 @@
+//code for saving transactions when offline
+//new db request for 'budget' db
+let db;
+
+const request = indexedDB.open('budget', 1);
+
+request.onupgradeneeded = function (event) {
+    const db = event.target.result;
+    db.createObjectStore('pending', {autoIncrement: true})
+};
+
+request.onsuccess = function (event) {
+    db = event.target.result;
+
+    if(navigator.onLine) {
+        checkDatabase();
+    }
+};
+
+request.onerror = function (event) {
+    console.log("Something went wrong! " + event.target.errorCode)
+};
+
+function saveTransaction(data) {
+    const transaction = db.transaction(["Pending"], "readWrite");
+
+    const store = transaction.objectStore("Pending");
+
+    store.add(data);
+};
+
+function checkDatabase() {
+    const transaction = db.transaction(["Pending"], "readWrite");
+
+    const store = transaction.objectStore("Pending");
+
+    const seeAll = store.getAll();
+
+    seeAll.onsuccess = function() {
+        if (seeAll.result.length > 0) {
+            fetch('/api/transaction/bulk', {
+                method: "POST",
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: "application/json, text/plain, */*",
+                    "Content-Type": "application/json"
+                }
+            })
+            .then(response => response.json())
+            .then(() => {
+                const transaction = db.transaction(["Pending"], "readwrite");
+
+                const store = transaction.objectStore('pending');
+
+                store.clear();
+            });
+        }
+    };
+}
+
+//listen for event - app coming back online
+window.addEventListener('online', checkDatabase);
+
+
