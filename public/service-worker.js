df47386ad2b3bf304a08bd9749e41388cfd0e629
Jules <juliannewood2413@gmail.com>
2021-02-11 10:30:24 -0700

    updated service worker, manifest, and index

--- public/service-worker.js
+++ public/service-worker.js
@@ -26,7 +26,7 @@ self.addEventListener('activate', function(evt) {
     evt.waitUntil(caches.keys()
     .then(keyList => {
         return Promise.all(
-            keyList.map(kay => {
+            keyList.map(key => {
                 if(key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
                     console.log("Removing old cache data", key);
                     return caches.delete(key);
